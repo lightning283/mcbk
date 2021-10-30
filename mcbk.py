fd77a1ba40e111a86896f6f2f7c2bbcb34f6785e
lightning283 <deadtarget283@gmail.com>
2021-10-30 23:07:17 +0530

    new improvements and open file explorer window when done

--- mcbk.py
+++ mcbk.py
@@ -14,7 +14,7 @@ def main():
 
     os.chdir(f"{mc_path}/saves")
 
-    if "backup" in sys.argv:
+    if sys.argv[1] == "backup":
         backup(f"{mc_path}/saves")
     try:
         if "upload" in sys.argv:
@@ -52,32 +52,35 @@ def anon_upload(filename):
         message = resp['error']['message']
         error_type = resp['error']['type']
         print(f'[ERROR] {message}\n{error_type}')
-
+def open_location():
+    if sys.platform == "win32":
+        os.system("explorer .")
+    elif sys.platform == "linux":
+        os.system("dolphin .")
 
 # makes backup zip of  : world , mods , shaders , etc
 def backup(mc_path):
-    mc_path = f"{mc_path}/saves"
     desktop_dir = Path.home().joinpath('Desktop')
 
-    arg = sys.argv
+    arg = sys.argv[2]
 
     target = None
 
-    if "world" in sys.argv:
+    if arg == "world":
         backup.world_name = input("Enter World Name: ")
         world_name = backup.world_name
         target = world_name
-    elif "mods" in arg:
-        os.chdir(mc_path)
-    elif "shaders" in arg:
+        make_archive(world_name, "zip", f"{mc_path}/{target}")
+        zipped_file = f"{mc_path}/{target}.zip"
+        os.system(f"cp -r '{zipped_file}' {desktop_dir}")
+        os.remove(zipped_file)
+        print("Backup located at Desktop : " + target)
+        os.chdir(desktop_dir)
+        open_location()
+    elif arg == "mods":
         pass
-
-    make_archive(world_name, "zip", f"{mc_path}/{target}")
-    zipped_file = f"{mc_path}/{target}.zip"
-
-    os.system(f"cp -r '{zipped_file}' {desktop_dir}")
-    print("Backup located at Desktop : " + target)
-
+    elif arg == "shaderpacks":
+        pass
 
 if __name__ == "__main__":
     main()
