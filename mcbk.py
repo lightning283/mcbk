7ab4c51301e54a55d48cea2fe381e634cb4a6615
lightning283 <deadtarget283@gmail.com>
2021-10-31 15:01:26 +0530

    fixed paths

--- mcbk.py
+++ mcbk.py
@@ -15,13 +15,14 @@ def main():
     os.chdir(f"{mc_path}/saves")
 
     if sys.argv[1] == "backup":
-        backup(f"{mc_path}/saves")
+        backup(mc_path)
     try:
         if "upload" in sys.argv:
             os.chdir(f"{mc_path}/saves")
             anon_upload(f"{backup.world_name}.zip")
     except IndexError:
         pass
+
 
 # returns minecraft path ( differs between operating system )
 def get_path():
@@ -52,35 +53,38 @@ def anon_upload(filename):
         message = resp['error']['message']
         error_type = resp['error']['type']
         print(f'[ERROR] {message}\n{error_type}')
+
+
 def open_location():
     if sys.platform == "win32":
         os.system("explorer .")
     elif sys.platform == "linux":
         os.system("dolphin .")
 
+
 # makes backup zip of  : world , mods , shaders , etc
 def backup(mc_path):
     desktop_dir = Path.home().joinpath('Desktop')
-
-    arg = sys.argv[2]
-
-    target = None
-
+    arg = sys.argv[2]                  
     if arg == "world":
+        if sys.platform == "linux":
+            mc_path = mc_path + '/saves'
+        else:
+            mc_path = mc_path + '\saves'
         backup.world_name = input("Enter World Name: ")
         world_name = backup.world_name
         target = world_name
-        make_archive(world_name, "zip", f"{mc_path}/{target}")
-        zipped_file = f"{mc_path}/{target}.zip"
-        os.system(f"cp -r '{zipped_file}' {desktop_dir}")
-        os.remove(zipped_file)
-        print("Backup located at Desktop : " + target)
-        os.chdir(desktop_dir)
-        open_location()
     elif arg == "mods":
         pass
     elif arg == "shaderpacks":
         pass
+    make_archive(target, "zip", f"{mc_path}/{target}")
+    zipped_file = f"{mc_path}/{target}.zip"
+    os.system(f"cp -r '{zipped_file}' {desktop_dir}")
+    os.remove(zipped_file)
+    print("Backup located at Desktop : " + target)
+    os.chdir(desktop_dir)
+    open_location()
 
 if __name__ == "__main__":
     main()
